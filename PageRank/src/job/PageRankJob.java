e208f20ace71f398dcd9ce1e7d82a96fa67f58d5
Joy Zhang <zyjd2008@yahoo.com.cn>
2013-03-20 17:49:25 +0800

    Beginning

---
+++ PageRank/src/job/PageRankJob.java
@@ -0,0 +1,17 @@
+package job;
+
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.*;
+
+public class PageRankJob {
+
+	
+	public static void main(String[] args) {
+		JobConf job = new JobConf();
+		
+		
+		
+
+	}
+
+}
